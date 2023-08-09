efaec656c5966eac49dffbe28649ec97619a6258
azkaaa <gcaptaintz@gmail.com>
2023-08-09 20:44:43 +0700

    fix error: debug syntax

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
-    debug: ['error'],
+    debug: { request: ['error'] },
     routes: {
       cors: {
         origin: ['*'],
